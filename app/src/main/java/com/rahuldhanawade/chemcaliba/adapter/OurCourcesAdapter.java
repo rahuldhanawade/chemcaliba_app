9cdeaf75306f4346fa4922eaee51e117a4d05a97
rahuldhanawade <rahuldhanawade25@gmail.com>
2023-06-10 13:20:52 +0530

    OurCourcesAdapter chnages

--- app/src/main/java/com/rahuldhanawade/chemcaliba/adapter/OurCourcesAdapter.java
+++ app/src/main/java/com/rahuldhanawade/chemcaliba/adapter/OurCourcesAdapter.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -47,9 +48,8 @@ public class OurCourcesAdapter extends RecyclerView.Adapter<OurCourcesAdapter.Vi
         OurCourcesPOJO ourCourcesPOJOS1= ourCourcesPOJOS.get(position);
         holder.tv_category_oc.setText(ourCourcesPOJOS1.getCourseCategoryName());
         holder.tv_course_category_info_oc.setText(ourCourcesPOJOS1.getCourseName());
-        holder.tv_start_date_oc.setText(ourCourcesPOJOS1.getCourseStartDate());
-        holder.tv_end_date_oc.setText(ourCourcesPOJOS1.getCourseStartDate());
-        holder.tv_duration_oc.setText(ourCourcesPOJOS1.getCourseDurationNumberOfDays() + "Days");
+        holder.tv_date_oc.setText(ourCourcesPOJOS1.getCourseStartDate()+" To "+ourCourcesPOJOS1.getCourseStartDate());
+        holder.tv_duration_oc.setText("("+ourCourcesPOJOS1.getCourseDurationNumberOfDays() + "Days)");
         holder.tv_actual_price_oc.setText("₹"+ourCourcesPOJOS1.getCourseActualPrice());
         holder.tv_actual_price_oc.setPaintFlags(holder.tv_actual_price_oc.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         holder.tv_sell_price_oc.setText("₹"+ourCourcesPOJOS1.getCourseSellPrice());
@@ -60,6 +60,25 @@ public class OurCourcesAdapter extends RecyclerView.Adapter<OurCourcesAdapter.Vi
                         .error(R.drawable.logo_chemcaliba))
                 .load(img_url)
                 .into(holder.iv_img_oc);
+        boolean is_bought = ourCourcesPOJOS1.getIsAlreadyBought();
+        boolean is_expired = ourCourcesPOJOS1.getIsCourseExpired();
+        if(!is_bought && !is_expired){
+            holder.linear_buy_now.setVisibility(View.VISIBLE);
+            holder.linear_purchased.setVisibility(View.GONE);
+            holder.linear_expired.setVisibility(View.GONE);
+        }else if(is_bought && !is_expired){
+            holder.linear_buy_now.setVisibility(View.GONE);
+            holder.linear_purchased.setVisibility(View.VISIBLE);
+            holder.linear_expired.setVisibility(View.GONE);
+        }else if(is_expired){
+            holder.linear_buy_now.setVisibility(View.GONE);
+            holder.linear_purchased.setVisibility(View.GONE);
+            holder.linear_expired.setVisibility(View.VISIBLE);
+        }else {
+            holder.linear_buy_now.setVisibility(View.GONE);
+            holder.linear_purchased.setVisibility(View.GONE);
+            holder.linear_expired.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -69,19 +88,22 @@ public class OurCourcesAdapter extends RecyclerView.Adapter<OurCourcesAdapter.Vi
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView tv_category_oc,tv_course_category_info_oc,tv_start_date_oc,tv_end_date_oc,tv_duration_oc,
+        TextView tv_category_oc,tv_course_category_info_oc,tv_date_oc,tv_duration_oc,
                 tv_actual_price_oc,tv_sell_price_oc;
         ImageView iv_img_oc;
+        LinearLayout linear_buy_now,linear_purchased,linear_expired;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tv_category_oc = itemView.findViewById(R.id.tv_category_oc);
             tv_course_category_info_oc = itemView.findViewById(R.id.tv_course_category_info_oc);
-            tv_start_date_oc = itemView.findViewById(R.id.tv_start_date_oc);
-            tv_end_date_oc = itemView.findViewById(R.id.tv_end_date_oc);
+            tv_date_oc = itemView.findViewById(R.id.tv_date_oc);
             tv_duration_oc = itemView.findViewById(R.id.tv_duration_oc);
             tv_actual_price_oc = itemView.findViewById(R.id.tv_actual_price_oc);
             tv_sell_price_oc = itemView.findViewById(R.id.tv_sell_price_oc);
             iv_img_oc = itemView.findViewById(R.id.iv_img_oc);
+            linear_buy_now = itemView.findViewById(R.id.linear_buy_now);
+            linear_purchased = itemView.findViewById(R.id.linear_purchased);
+            linear_expired = itemView.findViewById(R.id.linear_expired);
         }
     }
 }
