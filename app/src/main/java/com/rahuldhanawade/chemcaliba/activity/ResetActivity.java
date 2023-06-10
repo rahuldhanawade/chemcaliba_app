87da9a7bc905f901df8a82a5cb6f059ba40b007a
rahuldhanawade <rahuldhanawade25@gmail.com>
2023-06-11 02:38:08 +0530

    method added in ResetActivity

--- app/src/main/java/com/rahuldhanawade/chemcaliba/activity/ResetActivity.java
+++ app/src/main/java/com/rahuldhanawade/chemcaliba/activity/ResetActivity.java
@@ -1,16 +1,165 @@
 package com.rahuldhanawade.chemcaliba.activity;
 
+import static com.rahuldhanawade.chemcaliba.RestClient.RestClient.ROOT_URL;
+import static com.rahuldhanawade.chemcaliba.utills.CommonMethods.DisplayToastError;
+import static com.rahuldhanawade.chemcaliba.utills.CommonMethods.DisplayToastSuccess;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.rahuldhanawade.chemcaliba.R;
+import com.rahuldhanawade.chemcaliba.utills.LoadingDialog;
+import com.rahuldhanawade.chemcaliba.utills.MyValidator;
+import com.rahuldhanawade.chemcaliba.utills.UtilitySharedPreferences;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResetActivity extends AppCompatActivity {
+
+    private LoadingDialog loadingDialog;
+    LinearLayout linear_submit;
+    EditText edt_email_R;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset);
+
+        loadingDialog = new LoadingDialog(ResetActivity.this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_reset);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Forgot Password");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        
+        Init();
+    }
+
+    private void Init() {
+
+        edt_email_R = findViewById(R.id.edt_email_R);
+        linear_submit = findViewById(R.id.linear_submit);
+
+        edt_email_R.setText("jeetendratyagi40@gmail.com");
+        linear_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isValid()){
+                    ResetPassword();
+                } 
+            }
+        });
+    }
+
+    private void ResetPassword() {
+
+        String Str_email = edt_email_R.getText().toString();
+
+        loadingDialog.startLoadingDialog();
+
+        String Reset_URL = ROOT_URL+"forgot_password";
+
+        Log.d("Reset_URL",""+Reset_URL);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Reset_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loadingDialog.dismissDialog();
+                        try {
+                            JSONObject responseObj = new JSONObject(response);
+                            boolean status = responseObj.getBoolean("status");
+                            String message = responseObj.getString("message");
+                            if(status){
+
+                                DisplayToastSuccess(ResetActivity.this,message);
+
+                                Intent i = new Intent(ResetActivity.this, LoginActivity.class);
+                                startActivity(i);
+                            }else{
+                                DisplayToastError(ResetActivity.this,message);
+                            }
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loadingDialog.dismissDialog();
+
+                        if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                            DisplayToastError(ResetActivity.this,"Server is not connected to internet.");
+                        } else if (error instanceof AuthFailureError) {
+                            DisplayToastError(ResetActivity.this,"Server couldn't find the authenticated request.");
+                        } else if (error instanceof ServerError) {
+                            DisplayToastError(ResetActivity.this,"Server is not responding.Please try Again Later");
+                        } else if (error instanceof NetworkError) {
+                            DisplayToastError(ResetActivity.this,"Your device is not connected to internet.");
+                        } else if (error instanceof ParseError) {
+                            DisplayToastError(ResetActivity.this,"Parse Error (because of invalid json or xml).");
+                        }
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("emailid", Str_email);
+                Log.d("ResetParamas",""+map.toString());
+                return map;
+            }
+        };
+
+        int socketTimeout = 50000; //30 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private boolean isValid() {
+        boolean result = true;
+
+        if (!MyValidator.isValidEmail(edt_email_R)) {
+            edt_email_R.requestFocus();
+            DisplayToastError(ResetActivity.this,"Please Enter Valid Email Id");
+            result = false;
+        }
+
+        return result;
     }
 }
