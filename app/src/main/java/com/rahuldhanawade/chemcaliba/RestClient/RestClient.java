b279f022eac8b1d85dd3d7867e9888ccda3111d1
rahuldhanawade <rahuldhanawade25@gmail.com>
2023-06-18 16:30:01 +0530

    Minor url changes

--- app/src/main/java/com/rahuldhanawade/chemcaliba/RestClient/RestClient.java
+++ app/src/main/java/com/rahuldhanawade/chemcaliba/RestClient/RestClient.java
@@ -2,9 +2,11 @@ package com.rahuldhanawade.chemcaliba.RestClient;
 
 public class RestClient {
 
-    public static String ROOT_URL = "https://digi-udyog.in/student_portal_uat/api/";
-    public static String IMG_URL = "https://digi-udyog.in/student_portal_uat/uploads/course/";
-    public static String CHAP_DOC_URL = "https://digi-udyog.in/student_portal_uat/uploads/";
+    public static String BASE_URL = "https://digi-udyog.in/student_portal_uat/";
+
+    public static String ROOT_URL = BASE_URL+"api/";
+    public static String IMG_URL = BASE_URL+"/uploads/course/";
+    public static String CHAP_DOC_URL = BASE_URL+"/uploads/";
     public static String RAZORPAY_KEY = "rzp_test_4OaRHILaXqcwlp";
 
 }
